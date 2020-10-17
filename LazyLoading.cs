3f65dab739d3827f343672103f4c8083b270a0dc
SINGHAL Ayushi <ayushi.singhal84@gmail.com>
2020-10-17 21:52:58 +0530

    LazyLoading and screenshot

---
+++ LazyLoading.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTVAutomation
+{
+    [TestClass]
+    public class LazyLoading
+    {
+        [TestMethod]
+        public void TestLazyLoading()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            int scrollLimit = 5;
+            webDriver.Navigate().GoToUrl("https://onepagelove.com/tag/lazy-load");
+            webDriver.Manage().Window.Maximize();
+            ITakesScreenshot screenshotDriver = webDriver as ITakesScreenshot;
+
+
+            for (int i = scrollLimit; i >= 0; i--)
+            {
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("C:/temp/Screenshot"+i+".png", ScreenshotImageFormat.Png);
+
+                IJavaScriptExecutor js = (IJavaScriptExecutor)webDriver;
+                js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+            }
+            
+            IWebElement elem = webDriver.FindElement(By.XPath("//a[@title='The Rollercoaster Life']"));
+            var text = elem.Text;
+            elem.Click();
+            webDriver.Quit();
+
+        }
+        
+    }
+}
